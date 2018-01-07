cf60d3aa70ecd03b8de592f196ac16941d621b39
Sam Lau <esayemm@gmail.com>
2018-01-07 00:59:45 -0800

    export recommended config

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 'use strict'
 
 module.exports = {
+  configs: {
+    recommended: {
+      plugins: ['esayemm'],
+      rules: {
+        'esayemm/align-imports': 2,
+        'esayemm/sort-imports': 2,
+      },
+    },
+  },
   rules: {
     'align-imports': require('./rules/align-imports.js'),
     'sort-imports': require('./rules/sort-imports.js'),
